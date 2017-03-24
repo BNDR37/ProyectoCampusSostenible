41be7dc23732580d3a4e282a44c8f64c895fc2e3
BNDR37 <deicide@live.com>
2017-03-24 16:55:42 -0500

    Añadí el proyecto de Unity

---
+++ EjemploCampus/Assets/Scripts/camaraZoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraZoom : MonoBehaviour {
+
+
+	public GameObject camera;
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		camera.transform.position.Set();
+		
+	}
+}
